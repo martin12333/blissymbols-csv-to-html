c4428a1ff24c342d13fc8b7abb822710c8b71b6e
Martin Novy <martin12333@users.noreply.github.com>
2016-07-15 16:45:25 +0200

    rename csv to tsv

--- example-test.sh
+++ example-test.sh
@@ -1,2 +1,2 @@
-python bliss_csv_to_html_150.py <INPUT-1XX.csv >OUTPUT.html
-python bliss_csv_to_html_150.py <INPUT-1XX-another.csv >OUTPUT2.html
+python bliss_csv_to_html_150.py <INPUT-1XX.tsv >OUTPUT.html
+python bliss_csv_to_html_150.py <INPUT-1XX-another.tsv >OUTPUT2.html
